46905718761948c3a52ad5544f7f55ba40f1d9c5
inakt <231853424+inakt@users.noreply.github.com>
2025-09-12 20:40:27 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
+const CACHE_NAME = "countdown-cache-v1";
+const URLS = ["index.html","manifest.json","sw.js","data.json"];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(URLS)));
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
 
